045da942061b65f014c190f3cd44d3826f452b48
Jamison Ordway <jamisonordway@gmail.com>
2021-02-07 14:41:29 -0700

    Add create and read games functionality, add bosses to games

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_06_001333) do
+ActiveRecord::Schema.define(version: 2021_02_07_210432) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,13 @@ ActiveRecord::Schema.define(version: 2021_02_06_001333) do
   create_table "bosses", force: :cascade do |t|
     t.string "name"
     t.integer "difficulty"
+    t.bigint "game_id"
+    t.index ["game_id"], name: "index_bosses_on_game_id"
   end
 
+  create_table "games", force: :cascade do |t|
+    t.string "title"
+  end
+
+  add_foreign_key "bosses", "games"
 end
