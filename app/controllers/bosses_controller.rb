704d51710e2b83b6a6f2b71ac9a874c4b23bae71
Jamison Ordway <jamisonordway@gmail.com>
2021-02-07 13:44:09 -0700

    Add create boss functionality

--- app/controllers/bosses_controller.rb
+++ app/controllers/bosses_controller.rb
@@ -6,4 +6,19 @@ class BossesController < ApplicationController
 
   def new
   end
+
+  def create
+    boss_params = params.permit(:name, :difficulty)
+
+    boss = Boss.new(
+      name: boss_params[:name],
+      difficulty: boss_params[:difficulty]
+    )
+
+    if boss.save
+      redirect_to(bosses_path)
+    else
+      flash[:notice] = 'Error creating boss'
+    end
+  end
 end
